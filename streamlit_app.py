419624bf49b5dc40591dff357111582927c1c58c
shaikaneesahammad <40515206+shaikaneesahammad@users.noreply.github.com>
2025-12-17 00:04:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,7 +54,7 @@ if ingredients_list:
 
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
 
     
     
